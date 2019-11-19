d61804b0ed6b269cf6d2297b6a89b14af4b333b8
Emmet <40059395+Emmet1337@users.noreply.github.com>
2019-11-19 20:34:29 +0600

    Update tests.py

--- courses/tests.py
+++ courses/tests.py
@@ -9,59 +9,45 @@ from courses.models import Contact
 
 
 class CategoryModelTest(TestCase):
-    def create_category(self):
-        return Category.objects.create(
-            name='Gaming',
-            imgpath='BoomBamBapBarabaBoomPau',
-        )
-
-    def test_category_creation(self):
-        category = self.create_category()
-        self.assertEqual(category.name, 'Gaming')
-
-
-class CourseModelTest(TestCase):
-    def create_course(self):
+    def setUp(self):
         category = Category.objects.create(
             name='Gaming',
             imgpath='BoomBamBapBarabaBoomPau',
         )
-        return Course.objects.create(
+        course = Course.objects.create(
             name='Cooking',
             description='2number9number9largenumber6withextradeep2number45onewithcheese',
             category=category,
             logo='ddos.png',
         )
-
-    def test_course_creation(self):
-        course = self.create_course()
-        self.assertEqual(course.logo, "ddos.png")
-
-
-class BranchModelTest(TestCase):
-    def create_branch(self):
-        course = CourseModelTest.create_course(self)
-        return Branch.objects.create(
+        Branch.objects.create(
             latitude='453534535434534543',
             longitude='23424234234252352352',
             address='ul Svyazistov 10 kv 150',
             course=course
         )
-
-    def test_branch_creation(self):
-        branch = self.create_branch()
-        self.assertEqual(branch.address, "ul Svyazistov 10 kv 150")
-
-
-class ContactModelTest(TestCase):
-    def create_contact(self):
-        course = CourseModelTest.create_course(self)
-        return Contact.objects.create(
+        Contact.objects.create(
             type='FACEBOOK',
             value='dragosteadintei',
             course=course,
         )
 
+    def test_category_creation(self):
+        category = Category.objects.get(name='Gaming', imgpath='BoomBamBapBarabaBoomPau')
+        self.assertEqual(category.name, 'Gaming')
+        self.assertEqual(category.imgpath, 'BoomBamBapBarabaBoomPau')
+
+    def test_course_creation(self):
+        course = Course.objects.get(name='Cooking', logo='ddos.png')
+        self.assertEqual(course.name, 'Cooking')
+        self.assertEqual(course.logo, 'ddos.png')
+
+    def test_branch_creation(self):
+        branch = Branch.objects.get(latitude='453534535434534543', address='ul Svyazistov 10 kv 150')
+        self.assertEqual(branch.address, "ul Svyazistov 10 kv 150")
+        self.assertEqual(branch.latitude, "453534535434534543")
+
     def test_contact_creation(self):
-        contact = self.create_contact()
+        contact = Contact.objects.get(type='FACEBOOK', value='dragosteadintei',)
         self.assertEqual(contact.value, 'dragosteadintei')
+        self.assertEqual(contact.type, 'FACEBOOK')
